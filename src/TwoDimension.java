c9dfe8b2b825d8ea4d3194485945f7bbd2a5fab5
Fikireyesus Bancha <fikireyesusb@gmail.com>
2022-04-16 23:50:37 +0200

    Initial commit

---
+++ src/TwoDimension.java
@@ -0,0 +1,28 @@
+public class TwoDimension
+{
+
+        public static void main(String[] args) {
+            int n = 10, total = 0;
+            int[][] matrix = new int[n][n];
+
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix.length; j++) {
+                    if (i == j) {
+                        matrix[i][j] = i;
+                        total += matrix[i][j];
+                    }else{
+                        matrix[i][j] = 0;
+                    }
+                    System.out.println(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println();
+            System.out.println("The sum of the diagonals is: " + total);
+
+
+    }
+}
